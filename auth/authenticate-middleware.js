10d8d5bb8f6b83298253c4d063fd961247c95c56
David Moates <david.m.moates@gmail.com>
2020-05-01 20:44:48 -0500

    done with middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,33 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const users = require('../users/users-model')
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const restrict = () => {
+  const authError = {
+    message: "invalid Credentials",
+  }
+
+  return async (req, res, next) => {
+    try {
+      const { token } = req.cookies
+      if(!token) {
+        return res.status(401).json(authError)
+      }
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json(authError)
+        }
+
+        req.token = decoded
+        console.log(decoded)
+
+        next()
+
+      })
+    } catch(err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = restrict;
