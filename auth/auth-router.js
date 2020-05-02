d81a9159c5c2ce8bfb0acf6e64509b298b5a8635
David Moates <david.m.moates@gmail.com>
2020-05-01 20:37:45 -0500

    wrote out auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,51 @@
-const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const users =  require('../users/users-model')
+const restrict = require('./authenticate-middleware')
 
-router.post('/register', (req, res) => {
-  // implement registration
+const router = require('express').Router()
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const  { username } = req.body
+    const user = await users.findBy({ usersname }).first()
+
+    if(user) {
+      return res.status(409).json({
+        message: "Username is taken. Try another.",
+      })
+    }
+
+    res.status(201).json(await users.add(req.body))
+  } catch(err) {
+    next(err)
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await users.findBy({ username }).first()
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (!user || !passwordValid) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      })
+    }
+    const payload = {
+      userId: user.id,
+      userRole: "normal"
+    }
+    const token = jwt.sign(payload, process.env.JWT_SECRET)
+
+    res.cookie('token', token)
+    res.json({
+      message: `Welcome ${user.username}!`,
+    })
+  } catch(err) {
+    nex(err)
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const server = require('./api/server.js');
 
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
 const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
